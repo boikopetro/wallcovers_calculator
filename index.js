d94165a41e2423e51181a108d88a2f150bb46027
boikopetro <boikopetromail@gmail.com>
2021-08-03 18:32:28 +0000

    refactoring

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 document.getElementById("btn").addEventListener('click', result)
 function result() {
     const width = document.getElementById("width").value
-    const depth = document.getElementById("depth").value
+    const length = document.getElementById("length").value
     const heigth = document.getElementById("heigth").value
     const showResult = document.getElementById("result")
     const out = document.getElementById('out')  
     let data
     let radio = document.querySelectorAll('.i-7')
-    for (let i=0; i<radio.length; i++) {
+    for (let i = 0; i < radio.length; i++) {
         if(radio[i].checked) {
         data=radio[i].value
         }
     }
-        if(width<0 || depth<0 || heigth<0) {
+        if (width < 0 || length < 0 || heigth < 0) {
             alert("Невірно вказаний розмір!!!")
         }
-        if((width==='0' && depth==='0' && heigth==='0') || (width==='0' && depth==='0') || (width==='0'&& heigth==='0') || (depth==='0' && heigth==='0')) {
+        if ((width==='0' && length==='0' && heigth==='0') || (width==='0' && length==='0') || (width==='0'&& heigth==='0') || (length==='0' && heigth==='0')) {
             alert("Розмір не може дорівнювати нулю!!!")
         }
-        if((width==='' && depth==='' && heigth==='') || (width==='' && depth==='') || (width===''&& heigth==='') || (depth==='' && heigth==='')) {
+        if ((width==='' && length==='' && heigth==='') || (width==='' && length==='') || (width===''&& heigth==='') || (length==='' && heigth==='')) {
             alert("Розмір не може дорівнювати нулю!!!")
         }
-        if(width==='' || width==='0') {
-            showResult.innerHTML = "Загальна площа: "+(depth*heigth).toFixed(2) +" м.кв."
-            out.innerHTML = "Кількість рулонів: "+Math.ceil((depth*heigth).toFixed(2)/data) + " шт."
+        if (!width) {
+            showResult.innerHTML = "Загальна площа: " + (length * heigth).toFixed(2) + " м.кв."
+            out.innerHTML = "Кількість рулонів: " + Math.ceil((length * heigth).toFixed(2)/data) + " шт."
         }
-        if(depth==='' || depth==='0') {
-            showResult.innerHTML = "Загальна площа: "+(width*heigth).toFixed(2) +" м.кв."
-            out.innerHTML = "Кількість рулонів: "+Math.ceil((width*heigth).toFixed(2)/data) + " шт."
+        if (!length) {
+            showResult.innerHTML = "Загальна площа: " + (width * heigth).toFixed(2) +" м.кв."
+            out.innerHTML = "Кількість рулонів: " + Math.ceil((width * heigth).toFixed(2)/data) + " шт."
         }
-        if(heigth==='' || heigth==='0') {
-            showResult.innerHTML = "Загальна площа: "+(width*depth).toFixed(2) +" м.кв."
-            out.innerHTML = "Кількість рулонів: "+Math.ceil((width*depth).toFixed(2)/data) + " шт."
+        if (!heigth) {
+            showResult.innerHTML = "Загальна площа: "+(width * length).toFixed(2) +" м.кв."
+            out.innerHTML = "Кількість рулонів: " + Math.ceil((width * length).toFixed(2)/data) + " шт."
         }
-        if(width>0 && depth>0 && heigth>0) {
-            showResult.innerHTML = "Загальна площа: "+(((width*heigth)+(depth*heigth))).toFixed(2)*2+" м.кв."
-            out.innerHTML = "Кількість рулонів: "+ Math.ceil(((((width*heigth)+(depth*heigth))).toFixed(2)*2)/data) + " шт."
+        if (width > 0 && length > 0 && heigth > 0) {
+            showResult.innerHTML = "Загальна площа: " + (((width * heigth) + (length * heigth))).toFixed(2) * 2 + " м.кв."
+            out.innerHTML = "Кількість рулонів: " + Math.ceil(((((width * heigth) + (length * heigth))).toFixed(2) * 2)/data) + " шт."
         }
 }
 
